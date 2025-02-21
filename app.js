81ef2cd1b854e22a3fe5e88f42d8ffdca512146a
lhda <lehoangdaian@gmail.com>
2025-02-21 19:59:11 +0700

    Added first 2 features

--- app.js
+++ app.js
@@ -1,15 +1,61 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const fs = require("fs");
+const bodyParser = require("body-parser");
+const multer = require("multer");
+const upload = multer();
 const port = 3000;
+
+// for parsing application/json
+app.use(bodyParser.json());
+
+// for parsing application/xwww-
+app.use(bodyParser.urlencoded({ extended: true }));
+//form-urlencoded
+
+// for parsing multipart/form-data
+app.use(upload.array());
 
 app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "./src/views"));
 app.set("view engine", "ejs");
 app.get("/", (req, res) => {
-  res.render("index");
+	res.render("index");
+});
+
+app.post("/add", upload.any(), function (req, res) {
+	var path = __dirname + "/public/data.csv";
+	var data = "\n";
+	for (const [key, value] of Object.entries(req.body)) {
+		data += value + ",";
+	}
+	data = data.substring(0, data.length - 1);
+	fs.appendFile(path, data, function (err) {
+		console.log(err);
+		res.redirect("/");
+	});
+});
+
+app.post("/remove", upload.any(), function (req, res) {
+	var path = __dirname + "/public/data.csv";
+	var data = req.body.data;
+	fs.writeFile(path, data, function (err) {
+		console.log(err);
+		res.redirect("/");
+	});
+});
+
+app.post("/update", upload.any(), function (req, res) {
+	// var path = __dirname + "/public/data.csv";
+	// var data = req.body.data;
+	// fs.writeFile(path, data, function (err) {
+	// 	console.log(err);
+	// 	res.redirect("/");
+	// });
+	res.redirect("/");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+	console.log(`Example app listening on port ${port}`);
 });
--- public/script.js
+++ public/script.js
@@ -1,23 +1,113 @@
+var data = [[]];
+
 (async function () {
-  const csv = await fetch("data.csv");
-  const data = csv.text().then((text) => {
-    const rows = text.split("\n").map((row) => row.split(","));
-    const table = document.getElementById("table");
+	const csv = await fetch("data.csv");
+	csv.text().then((text) => {
+		data = text.split("\n").map((row) => row.split(","));
+		const table = document.getElementById("table");
 
-    table.innerHTML = "";
+		table.innerHTML = "";
 
-    for (let i = 0; i < rows.length; i++) {
-      let tr = document.createElement("tr");
+		for (let i = 0; i < data.length; i++) {
+			let tr = document.createElement("tr");
 
-      for (let j = 0; j < rows[i].length; j++) {
-        let td = document.createElement("td");
+			for (let j = 0; j < data[i].length; j++) {
+				let td = document.createElement("td");
 
-        td.textContent = rows[i][j];
-        tr.appendChild(td);
-      }
-      table.appendChild(tr);
-    }
-
-    button.style.display = "block";
-  });
+				td.textContent = data[i][j];
+				tr.appendChild(td);
+			}
+			table.appendChild(tr);
+		}
+	});
 })();
+
+var modal = document.getElementById("inputModal");
+var modalContent = document.getElementById("inputModalContent");
+var inputForm = document.getElementById("inputForm");
+var span = document.getElementsByClassName("close")[0];
+
+var btnAdd = document.getElementById("btnAdd");
+
+var btnRemove = document.getElementById("btnRemove");
+var inputRemove = document.getElementById("inputRemove");
+
+var btnUpdate = document.getElementById("btnUpdate");
+var inputUpdate = document.getElementById("inputUpdate");
+
+btnAdd.onclick = addStudent;
+btnRemove.onclick = removeStudent;
+btnUpdate.onclick = updateStudent;
+span.onclick = closeModal;
+window.onclick = function (event) {
+	if (event.target == modal) {
+		closeModal();
+	}
+};
+
+function openModal() {
+	modal.style.display = "block";
+}
+
+function closeModal() {
+	modal.style.display = "none";
+}
+
+function addStudent() {
+	inputForm.setAttribute("action", "/add");
+	openModal();
+}
+
+function removeStudent() {
+	for (let index = 1; index < data.length; index++) {
+		const row = data[index];
+		// console.log(row[0]);
+
+		if (row[0] == inputRemove.value) {
+			data.splice(index, 1);
+
+			fetch("/remove", {
+				method: "POST",
+				body: JSON.stringify({
+					data: dataToString(),
+				}),
+				headers: {
+					"Content-type": "application/json; charset=utf-8",
+				},
+			}).then((window.location.href = "/"));
+
+			return;
+		}
+	}
+	window.alert("MSSV không tồn tại!");
+}
+
+function updateStudent() {
+	inputForm.setAttribute("action", "/update");
+
+	var inputs = inputForm.querySelectorAll("input, select");
+
+	data.forEach((row) => {
+		if (row[0] == inputUpdate.value) {
+			for (let index = 0; index < row.length; index++) {
+				if (inputs[index].value !== undefined) {
+					inputs[index].value = row[index];
+				}
+			}
+			openModal();
+			return;
+		}
+	});
+}
+
+function dataToString() {
+	var string = "";
+	for (let index = 0; index < data.length; index++) {
+		const row = data[index];
+		string += row.toString() + "\n";
+	}
+
+	string = string.substring(0, string.length - 1);
+
+	return string;
+}
